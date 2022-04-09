8cfab8d88a70d3afa982f144ff28584e479c6306
kibra <62299912+kibradev@users.noreply.github.com>
2022-04-09 10:59:21 +0300

    Add files via upload

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,15 @@
 $(function() {
     window.addEventListener('message', function(event) {
         if (event.data.type === "SendInfo"){
-            $('.MainClass').css({"animation":"fadeInBottom 0.8s both"});
+            $('.MainClass').slideDown("slow");
             $('.MainClass').css({"display":"flex"});
             $('.MainClass').fadeIn(500);
             $('#Button').html(event.data.ButtonKey);
             $('#UIText').html(event.data.Text);
         } else if(event.data.type === "CloseUI") {
-            $('.MainClass').css({"animation":"fadeOutBottom 0.8s both"});
+            $('.MainClass').slideUp("slow");
             setTimeout(() => {
-                $('.MainClass').css({"display":"none"});
+                $('.MainClass').slideUp("slow");
                 $('.MainClass').hide();
             }, 1000);
         }
